9c14b6e59441d56d54623cdcdde5fbec8d1109a0
Jeff Hykin <jeff.hykin@gmail.com>
2025-08-01 09:57:41 -0500

    testing flake.nix

--- flake.nix
+++ flake.nix
@@ -5,9 +5,10 @@
         # nixpkgs.url = "github:NixOS/nixpkgs/6f884c2#nodejs-slim";
         # nixpkgsWithPython38.url = "https://github.com/NixOS/nixpkgs/archive/9108a20782535741433c304f6a4376cb8b364b89.tar.gz";
         nixpkgsWithNodejs16.url = "https://github.com/NixOS/nixpkgs/archive/a71323f68d4377d12c04a5410e214495ec598d4c.tar.gz";
+        nixpkgsWithRuby.url = "https://github.com/NixOS/nixpkgs/archive/ebf88190cce9a092f9c7abe195548057a0273e51.tar.gz";
         xome.url = "github:jeff-hykin/xome";
     };
-    outputs = { self, nixpkgs, nixpkgsWithNodejs16, xome, ... }:
+    outputs = { self, nixpkgs, nixpkgsWithNodejs16, nixpkgsWithRuby, xome, ... }:
         xome.superSimpleMakeHome { inherit nixpkgs; pure = true; } ({system, ...}:
             let
                 setup = {
@@ -26,6 +27,7 @@
                 pkgs = import nixpkgs setup;
                 # pkgsWithPython38 = import nixpkgsWithPython38 setup;
                 pkgsWithNodejs16 = import nixpkgsWithNodejs16 setup;
+                pkgsWithRuby = import nixpkgsWithRuby setup;
             in
                 {
                     # for home-manager examples, see: https://deepwiki.com/nix-community/home-manager/5-configuration-examples
@@ -38,6 +40,7 @@
                         pkgs.coreutils-full
                         
                         # optional stuff
+                        pkgs.bash
                         pkgs.gnugrep
                         pkgs.findutils
                         pkgs.wget
@@ -58,7 +61,7 @@
                         pkgs.git
                         
                         # project specific
-                        pkgsWithNodejs16.nodejs # v16.15.0
+                        pkgsWithNodejs16.nodejs_16 # v16.15.0
                         # npm --version           8.5.5
                         # pkgsWithPython38.python38 # 3.8.13.venv
                         # pkgsWithPython38.python38Packages.setuptools
@@ -69,11 +72,11 @@
                         pkgs.cmake
                         pkgs.pkg-config
                         pkgs.libffi
-                        pkgs.ruby.devEnv # ruby 2.7.6p219 (2022-04-12 revision c9c2245c0a) [arm64-darwin21]
-                        pkgs.bundix
-                        pkgs.sqlite
-                        pkgs.libpcap
-                        pkgs.postgresql
+                        pkgsWithRuby.ruby.devEnv # ruby 2.7.6p219 (2022-04-12 revision c9c2245c0a) [arm64-darwin21]
+                        pkgsWithRuby.bundix
+                        pkgsWithRuby.sqlite
+                        pkgsWithRuby.libpcap
+                        pkgsWithRuby.postgresql
                         pkgs.libxml2
                         pkgs.libxslt
                         pkgs.pkg-config
@@ -97,6 +100,25 @@
                             initContent = ''
                                 # this enables some impure stuff like sudo, comment it out to get FULL purity
                                 export PATH="$PATH:/usr/bin/"
+                                
+                                #
+                                # Ruby setup
+                                #
+                                export GEM_HOME="$HOME/gems.ignore/"
+                                # if not setup yet, then setup ruby
+                                if ! [ -d "$VAR" ]
+                                then
+                                    mkdir "$GEM_HOME" &>/dev/null
+                                    bundix -l
+                                    bundler install
+                                fi
+                                
+                                #
+                                # Npm setup
+                                #
+                                if ! [ -d "./node_modules" ]; then
+                                    npm install
+                                fi
                             '';
                         };
                         starship = {
